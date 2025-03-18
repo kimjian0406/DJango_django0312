d415dcd554a2e1e345f85f598f6db0e3355da7f0
kimjian0406 <oheunsuhhy3@naver.com>
2025-03-18 11:10:59 +0900

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('signup/', views.sign_up, name='signup'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', include('django.contrib.auth.urls')),  # 로그아웃 URL 추가
-]
-
