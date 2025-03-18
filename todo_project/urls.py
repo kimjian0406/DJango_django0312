c48cb344511177a0a50e6f540d1fa208e0c7428f
이라온 <ilaon@ilaon-ui-MacBookPro.local>
2025-03-18 13:19:51 +0900

    Add new features for todo app (models, views, forms)

---
+++ todo_project/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('todo/create/', views.todo_create, name='todo_create'),
+    path('todo/<int:todo_id>/update/', views.todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete/', views.todo_delete, name='todo_delete'),
+    path('todo/', views.todo_list, name='todo_list'),
+]
+
